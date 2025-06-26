2d2edae05ee40fda9ec37d0944d07c78889bdf8c
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-06-26 19:34:36 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: { keyframes: {
+        'horizontal-scroll': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-50%)' }
+        },
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateX(20px)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' }
+        }
+      },
+      animation: {
+        'horizontal-scroll': 'horizontal-scroll 20s linear infinite',
+        'fadeInUp': 'fadeInUp 0.5s ease-out forwards'
+      }},
   },
-  plugins: [],
+  
+  plugins: [
+      require('@tailwindcss/forms'),
+      require('@tailwindcss/typography'),
+      require('@tailwindcss/aspect-ratio'),
+  ],
 };
