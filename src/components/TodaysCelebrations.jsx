9951ff34593f5fb27ceeda2df2b91e49c05dacfb
Bhuvaneshmaker <105157420+Bhuvaneshmaker@users.noreply.github.com>
2025-06-27 10:48:55 +0530

    Update TodaysCelebrations.jsx

--- src/components/TodaysCelebrations.jsx
+++ src/components/TodaysCelebrations.jsx
@@ -11,9 +11,9 @@ const TodaysCelebrations = ({ todaysBirthdays, todayJoin, calculateAge, getYears
             <Star className="text-yellow-300 animate-spin" />
             🎉 Today's Birthday Celebrations! 🎉  <Heart className='text-red-300 animate-bounce'/>
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2  gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2  flex items-center justify-center gap-4">
             {todaysBirthdays.map((emp, index) => (
-              <div key={index} className="bg-black bg-opacity-20 rounded-lg flex items-center justify-center p-4">
+              <div key={index} className="bg-black bg-opacity-20 rounded-lg p-4">
                 <h4 className="font-bold text-xl">{emp.name}</h4>
                 <p className="text-pink-100">🎂 Turning {calculateAge(emp.birthday)} years old!</p>
                 <div className="mt-2 p-2 bg-white bg-opacity-10 rounded">
@@ -34,9 +34,9 @@ const TodaysCelebrations = ({ todaysBirthdays, todayJoin, calculateAge, getYears
             <Star className="text-yellow-300 animate-spin" />
             🎉 Work Anniversary Celebrations! 🎉<Heart className='text-red-300 animate-bounce'/>
           </h2>
-          <div className="grid grid-cols-1 md:grid-cols-2   gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 flex items-center justify-center  gap-4">
             {todayJoin.map((emp, index) => (
-              <div key={index} className="bg-white text-black bg-opacity-20 flex items-center justify-center rounded-lg p-4">
+              <div key={index} className="bg-white text-black bg-opacity-20  rounded-lg p-4">
                 <h4 className="font-bold text-xl text-black">{emp.name}</h4>
                 <p className="text-green-700">🎉 Stepping to {getYearsOfService(emp.joinDate)} years!</p>
                 <div className="mt-2 p-2 bg-grey-200 bg-opacity-10 rounded">
